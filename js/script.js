a461e7e91753ab7ffcd9c6f2946fed4734f1bb3b
Yusuf Yildiz <yusuf_yildiz@student.uml.edu>
2016-11-15 23:25:48 -0500

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
+/*   Name: Yusuf Yildiz
+     Email: GUI I
+     Date: November 11, 2016
+     Assignment No. 7
+     This webpage allows the user to create a dynamic multiplication table.
+     */
+
 $(document).ready(function(){
     $("#inputs").validate({
         rules: {
+            // Make sure that the first input isn't bigger than the second
             first: {
                 required: true,
                 digits: true,
                 range: [0, parseInt($("#second").val())]
             },
+            // Make sure that the second input is bigger than the first
             second: {
                 required: true,
                 digits: true,
                 range: [parseInt($("#first").val()), Number.MAX_SAFE_INTEGER]
             },
+            // Make sure that the third input isn't bigger than the fourth
             third: {
                 required: true,
                 digits: true,
                 range: [0, parseInt($("#fourth").val())]
             },
+            // Make sure that the fourth input is bigger than the third
             fourth: {
                 required: true,
                 digits: true,
@@ -24,13 +35,18 @@ $(document).ready(function(){
             }
         }
     });
+    // Stop the default behavior of the submit button
     $("#inputs").on("submit", function() {
         return false;
     });
+
     $("#inputs .btn").click(function() {
+        // Make sure that all inputs are valid before continuing
         if(!$("#inputs").valid()) {
             return;
         }
+
+
         var first = parseInt($("#first").val());
         var second = parseInt($("#second").val());
         var third = parseInt($("#third").val());
@@ -38,10 +54,6 @@ $(document).ready(function(){
 
         // Remove the previous table since we want a fresh table each time
         $(".table table").remove();
-
-        // Hide popus if they were showing
-        $(".popup").hide();
-        $(".popup-range").hide();
 
         // Create out table
         $(".table").append("<table>");
